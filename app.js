1bad2f3c4807ab5c0c620b75066f290551fb2bff
Marcelo Mendes <>
2021-09-01 07:35:02 -0300

    testes post

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+//onst bodyParser = require("body-parser");
+
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Indrodução a API");
 });
 
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/post", (req, res) => {
-    return res.json(req.body);
+    //return 'ok';
+    return res.json({teste:"ok"});
+});
+
+app.post("/post2", (req, res) => {
+    return res.json({titulo: "teste"});
 });
 
 app.listen(3333, () => {
