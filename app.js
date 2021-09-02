c41f47a04c0e591658867ae0cb5677a2e7548193
Marcelo Mendes <>
2021-09-02 17:28:49 -0300

    debug

--- app.js
+++ app.js
@@ -14,161 +14,156 @@ var os = require('os');
 //app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
-    // '/reports',
-    express.json(),
-    // express.static('reports'),
-    // serveIndex('/reports', {icons: true})
+    '/reports',
+    // express.json(),
+    express.static('reports'),
+    serveIndex('/reports', {icons: true})
     );
 
-app.get("/", (req, res) => {
-    res.send("Indrodução a API");
-});
+// app.get("/", (req, res) => {
+//     res.send("Indrodução a API");
+// });
 
-app.get("/pdf", (req, res) => {
+// app.get("/pdf", (req, res) => {
     
-});
+// });
 
-app.post("/post", (req, res) => {
-    //return 'ok';
-    return res.json({teste:"ok"});
-});
+// app.post("/post", (req, res) => {
+//     //return 'ok';
+//     return res.json({teste:"ok"});
+// });
 
-app.post("/post2", (req, res) => {
-    return res.json(req.body.key);
-});
+// app.post("/post2", (req, res) => {
+//     return res.json(req.body.key);
+// });
 
-app.post("/pref", (req, res) => {
-    (async () => {
+// app.post("/pref", (req, res) => {
+//     (async () => {
 
-      // if(os.hostname().indexOf("local") > -1){
-      //     const browser = await puppeteer.launch({
-      //       headless: true,
-      //     });
-      //   }else{
-      //     const browser = await puppeteer.launch({
-      //       executablePath: '/usr/bin/chromium-browser',
-      //     headless: true,
-      //   });
-      // }
+//       // if(os.hostname().indexOf("local") > -1){
+//       //     const browser = await puppeteer.launch({
+//       //       headless: true,
+//       //     });
+//       //   }else{
+//       //     const browser = await puppeteer.launch({
+//       //       executablePath: '/usr/bin/chromium-browser',
+//       //     headless: true,
+//       //   });
+//       // }
       
-      const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser',
-      headless: true,
-    });
-        const page = await browser.newPage();
+//       const browser = await puppeteer.launch({
+//         executablePath: '/usr/bin/chromium-browser',
+//       headless: true,
+//     });
+//         const page = await browser.newPage();
       
       
-         console.log('iniciando...');
-         res.send("Iniciando a API");
+//          console.log('iniciando...');
+//          res.send("Iniciando a API");
 
-         console.log(req.body);
+//          console.log(req.body);
         
-        // const url1 = req.body.url1;
-        // const url2 = req.body.url2;
-        await page.goto(req.body.url2);
-        console.log('passando pela url 1...');
+//         // const url1 = req.body.url1;
+//         // const url2 = req.body.url2;
+//         await page.goto(req.body.url2);
+//         console.log('passando pela url 1...');
       
-        await delay(3000);
-        console.log('delay de 3 segundos...');
+//         await delay(3000);
+//         console.log('delay de 3 segundos...');
       
-        // const downloadUrl = req.body.url3;
-        // const downloadUrl2 = req.body.url4;
+//         // const downloadUrl = req.body.url3;
+//         // const downloadUrl2 = req.body.url4;
       
-        await page.goto(req.body.url4);
+//         await page.goto(req.body.url4);
       
-        /**
-      * @param {page from where you want to download file } page
-      * @param {downloadLocation , directory where you want to save a file } downloadLocation
-      */
-      async function downloadFile(page, downloadLocation){
-        console.log('criando diretório '+downloadLocation+'...');
-          const downloadPath = path.resolve(downloadLocation)
-          mkdirp(downloadPath)
-        //   console.log('Downloading file to:', downloadPath)
-          await page._client.send('Page.setDownloadBehavior', {
-              behavior: 'allow',
-              downloadPath: downloadPath,
-              })
-      }
+//         /**
+//       * @param {page from where you want to download file } page
+//       * @param {downloadLocation , directory where you want to save a file } downloadLocation
+//       */
+//       async function downloadFile(page, downloadLocation){
+//         console.log('criando diretório '+downloadLocation+'...');
+//           const downloadPath = path.resolve(downloadLocation)
+//           mkdirp(downloadPath)
+//         //   console.log('Downloading file to:', downloadPath)
+//           await page._client.send('Page.setDownloadBehavior', {
+//               behavior: 'allow',
+//               downloadPath: downloadPath,
+//               })
+//       }
       
-      /**
-      * @param {url} url of your file
-      * @param {downloadLocation , directory where you want to save a file with file name and extenshion} downloadLocation
-       */
-      async function downloadFileFromURL(url, downloadLocation){
-          try {
-              const fetch = require('node-fetch');
-              const { writeFile } = require('fs');
-              const { promisify } = require('util');
-              const writeFilePromise = promisify(writeFile);
-              await fetch(url)
-                  .then(x => x.arrayBuffer())
-                  .then(x => {writeFilePromise(downloadLocation, Buffer.from(x));console.log(`File Downlaoded From URL: ${downloadLocation}`)});
-          } catch (error) {
-              throw new Error(error)
-          }
-      }
+//       /**
+//       * @param {url} url of your file
+//       * @param {downloadLocation , directory where you want to save a file with file name and extenshion} downloadLocation
+//        */
+//       async function downloadFileFromURL(url, downloadLocation){
+//           try {
+//               const fetch = require('node-fetch');
+//               const { writeFile } = require('fs');
+//               const { promisify } = require('util');
+//               const writeFilePromise = promisify(writeFile);
+//               await fetch(url)
+//                   .then(x => x.arrayBuffer())
+//                   .then(x => {writeFilePromise(downloadLocation, Buffer.from(x));console.log(`File Downlaoded From URL: ${downloadLocation}`)});
+//           } catch (error) {
+//               throw new Error(error)
+//           }
+//       }
      
-    //   const directoryFiles = 'reports';
+//     //   const directoryFiles = 'reports';
       
-      await downloadFile(page, req.body.directoryFiles);
+//       await downloadFile(page, req.body.directoryFiles);
       
-        // console.log('passando pela url 2...');
-        // console.log('pagina carregada...');  
+//         // console.log('passando pela url 2...');
+//         // console.log('pagina carregada...');  
 
-        console.log('informando credenciais...');
-        await page.type('[name="User"]', req.body.user1);
-        await page.type('[name="Password"]', req.body.pass2);
-        await page.click('[value="View Report"]');
+//         console.log('informando credenciais...');
+//         await page.type('[name="User"]', req.body.user1);
+//         await page.type('[name="Password"]', req.body.pass2);
+//         await page.click('[value="View Report"]');
       
-        // console.log('finalizando...');
+//         // console.log('finalizando...');
       
-        await delay(5000);
-        console.log('delay de 5 segundos...');
+//         await delay(5000);
+//         console.log('delay de 5 segundos...');
       
-        // const baseDirectory = 'D:/wamp/www/nodejs/logando-no-site/';
-        // const file = req.body.file;
+//         // const baseDirectory = 'D:/wamp/www/nodejs/logando-no-site/';
+//         // const file = req.body.file;
       
-        fs.rename(__dirname+'/'+req.body.directoryFiles+'/reportviewer.pdf', __dirname+'/'+req.body.directoryFiles+'/'+req.body.file, function(err) {
-          if ( err ){ 
-              console.log('ERROR: ' + err);
-              return;
-            }
-      });
+//         fs.rename(__dirname+'/'+req.body.directoryFiles+'/reportviewer.pdf', __dirname+'/'+req.body.directoryFiles+'/'+req.body.file, function(err) {
+//           if ( err ){ 
+//               console.log('ERROR: ' + err);
+//               return;
+//             }
+//       });
 
-      console.log('fechando browser...');
-      await browser.close();
+//       console.log('fechando browser...');
+//       await browser.close();
 
-      const pathFile = __dirname+'/'+req.body.directoryFiles+'/'+req.body.file;
+//       const pathFile = __dirname+'/'+req.body.directoryFiles+'/'+req.body.file;
 
-    try {
-        console.log('entrando no try...');
-        if (fs.existsSync(pathFile)) {
-            // return res.json({teste:"ok"});
-            console.log("Relatório gerado com sucesso");
+//     try {
+//         console.log('entrando no try...');
+//         if (fs.existsSync(pathFile)) {
+//             // return res.json({teste:"ok"});
+//             console.log("Relatório gerado com sucesso");
 
-            const ftp = require('./FTPClient');
-            const client = new ftp('ftp.detesolucoes.com.br', 21, 'rootftp@detesolucoes.com.br', '%M+DQc83=W6#', false);
-
-            client.upload(pathFile, 'public_html/'+req.body.file, 755);
-
-            return res.json({status: "success", message: "Relatório gerado com sucesso", file: pathFile, error: null});
-        }
-    } catch(err) {
-        console.log('entrando no catch...');
-        console.log(pathFile);
-        console.error("Ops! "+err)
+//             return res.json({status: "success", message: "Relatório gerado com sucesso", file: pathFile, error: null});
+//         }
+//     } catch(err) {
+//         console.log('entrando no catch...');
+//         console.log(pathFile);
+//         console.error("Ops! "+err)
         
-        return res.json({status: "error", message: "Erro ao gerar relatório", file: null, error: err});
-    }
+//         return res.json({status: "error", message: "Erro ao gerar relatório", file: null, error: err});
+//     }
       
         
-      })();
+//       })();
 
       
     
     
-});
+// });
 
 app.listen(3333, () => {
     console.log("Servidor iniciado na porta.");
